b499216b5fad4d190699f9ab8699151513df4e5c
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-08-12 22:47:08 +0530

    upload to ipfs using web3.storage
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
-
+import { useState } from 'react';
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
 
 
 export default function Home() {
 
+  const [fileUrl, setFileUrl] = useState("");
+
+  const handleUpload= async (e)=>{
+    const token = process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN_KEY
+
+    e.preventDefault();
+    const data = e.target.files[0];
+    
+    const storage = new Web3Storage({ token });
+    const files = []
+    files.push(data);
+
+    const cid = await storage.put(files)
+
+    setFileUrl(`ipfs://${cid}/${data.name}`);
+    // save file names,cid in json to get unique link 
+    
+  }
 
 
   return (
   <div>
       Hello
+      <br/>
+      <input type="file" onChange={handleUpload}/>
+      <br/>
+      {fileUrl && <p>{fileUrl}</p>}
+
   </div>
   )
 }
